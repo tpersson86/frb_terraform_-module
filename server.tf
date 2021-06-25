b26b37fdb584628ea41465c3a01867591347ddaa
Tom Persson <thomaspersson86@gmail.com>
2021-06-25 08:45:00 -0400

    Update server.tf

--- server.tf
+++ server.tf
@@ -15,7 +15,6 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.vpc_security_group_ids
-  }
 
   tags = {
     "Identity"    = var.identity
